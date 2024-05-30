395fb1b8966813dace9ed87c5fe25d2f455eaeba
Zachary Roy <zachary.roy@water.ca.gov>
2024-05-30 11:23:13 -0700

    Add new plotting functions

--- calsim_dash_widgets/plotting.py
+++ calsim_dash_widgets/plotting.py
@@ -1,10 +1,11 @@
 import dash
+import numpy as np
 import pandas as pd
 import plotly.express as px
 
 
-def sparkline(df: pd.DataFrame, **layout_kwargs) -> dash.dcc.Graph:
-    fig = px.line(x=df.index, y=df.iloc[:, 0])
+def sparkline(s: pd.Series, **layout_kwargs) -> dash.dcc.Graph:
+    fig = px.line(x=s.index, y=s.values)
     # hide and lock down axes
     fig.update_xaxes(visible=False, fixedrange=True)
     fig.update_yaxes(visible=False, fixedrange=True)
@@ -28,3 +29,45 @@ def sparkline(df: pd.DataFrame, **layout_kwargs) -> dash.dcc.Graph:
         figure=fig,
         config=dict(displayModeBar=False),
     )
+
+
+def exceedance(s: pd.Series, **layout_kwargs) -> dash.dcc.Graph:
+    s = s.sort_values(ascending=False)
+    e = np.arange(1.0, s.size + 1) / s.size
+    fig = px.line(x=s, y=e)
+    layout_kwargs = (
+        dict(
+            showlegend=False,
+            autosize=False,
+            width=750,
+            height=400,
+            xaxis_title="Value",
+            yaxis_title="Exceedance Probability",
+            font=dict(size=11),
+            yaxis=dict(
+                tickformat=".0%",
+                autorange="reversed",
+            ),
+        )
+        | layout_kwargs
+    )
+    fig.update_layout(**layout_kwargs)
+    return dash.dcc.Graph(figure=fig)
+
+
+def timeseries(s: pd.Series, **layout_kwargs) -> dash.dcc.Graph:
+    fig = px.line(x=s.index, y=s.values)
+    layout_kwargs = (
+        dict(
+            showlegend=False,
+            autosize=False,
+            width=750,
+            height=400,
+            xaxis_title="Date",
+            yaxis_title="Value",
+            font=dict(size=11),
+        )
+        | layout_kwargs
+    )
+    fig.update_layout(**layout_kwargs)
+    return dash.dcc.Graph(figure=fig)
